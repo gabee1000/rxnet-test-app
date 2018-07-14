5c4fd71e1f1adead5997f7e19a3cf2420eef616a
Gabor <gabee5000@gmail.com>
2018-07-14 12:17:52 +0200

    Added implementation of unsubscription test.

---
+++ ConsoleApp1/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Subjects;
+using RxTestApp;
+
+namespace RxTestApp {
+
+    class MainClass {
+
+        static void Main(string[] args) {
+            var reactive = new Reactive();
+            reactive.AsyncSubjectTest();
+            reactive.SubscriptionTest();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Reactive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Subjects;
+using System.Text;
+
+namespace RxTestApp {
+
+    class Reactive {
+
+        public Reactive() { }
+
+        public void AsyncSubjectTest() {
+            //todo
+        }
+
+        //The correct way to handle exceptions is to provide a delegate for OnError notifications as in this example.
+        public void SubscriptionTest() {
+            var values = new Subject<int>();
+            values.Subscribe(
+                value => Console.WriteLine("1st subscription received {0}", value),
+                ex => Console.WriteLine("Caught an exception : {0}", ex));
+            values.OnNext(0);
+            values.OnError(new Exception("Dummy exception!"));
+        }
+
+        public void UnsubscribeTest() {
+            var values = new Subject<int>();
+            var firstSubscription = values.Subscribe(value =>
+            Console.WriteLine("1st subscription received {0}", value));
+            var secondSubscription = values.Subscribe(value =>
+            Console.WriteLine("2nd subscription received {0}", value));
+            values.OnNext(0);
+            values.OnNext(1);
+            values.OnNext(2);
+            values.OnNext(3);
+            firstSubscription.Dispose();
+            Console.WriteLine("Disposed of 1st subscription");
+            values.OnNext(4);
+            values.OnNext(5);
+        }
+    }
+}
